970b0adfc803b306c7732dc76419d1c7271d389a
Thijs Luyten <99009420+DeltionThijs@users.noreply.github.com>
2023-03-14 17:44:27 +0100

    Update vulnerableIPscanner.py

--- vulnerableIPscanner.py
+++ vulnerableIPscanner.py
@@ -1,1 +1,29 @@
-import requests<br/>import re<br/>for i in range(2):<br/>    #?query=docker port:4243 is for searching docker containers on port 4243 in that url<br/>    url = f"https://api.criminalip.io/v1/banner/search?query=docker port:{i}&offset=0"<div></div>    payload = {}<br/>    #API key to query data from criminalIP<br/>    headers = {<br/>        "x-api-key": "LWRK89CgBen0k0BGxprqO0Nc7tHZMTamTCulT65Ep8e9req2mfAMHD3cscLe"<br/>    }<br/>    # The response from criminalIP<br/>    response = requests.request("GET", url, headers=headers, data=payload)<div></div>    # Write the found IP's response to a file<br/>    with open(f"output.txt", "w") as f:<br/>        f.write(response.text)<br/>    #Writing to a file named "output.txt"<br/>    print("Vulnerable IP's written to file 'output.txt'")<div></div>    with open(f"output.txt", "r") as f:<br/>        text = f.read()<br/>    #Filter for only getting the IP's out of the JSON data<br/>    ip_pattern = re.compile(r"\b(?:[0-9]{1,3}.){3}[0-9]{1,3}\b")<br/>    ip_addresses = re.findall(ip_pattern, text)<div></div>    # Writing it again to output.txt<br/>    with open(f"output.txt", "w") as f:<br/>        for ip in ip_addresses:<br/>            text = f.write(ip + "\n")
+import requests
+import re
+#?query=docker port:4243 is for searching docker containers on port 4243 in that url
+url = "https://api.criminalip.io/v1/banner/search?query=docker port:4243&offset=0"
+
+payload = {}
+#API key to query data from criminalIP
+headers = {
+    "x-api-key": "LWRK89CgBen0k0BGxprqO0Nc7tHZMTamTCulT65Ep8e9req2mfAMHD3cscLe"
+}
+# The response from criminalIP
+response = requests.request("GET", url, headers=headers, data=payload)
+
+# Write the found IP's response to a file
+with open("output.txt", "w") as f:
+    f.write(response.text)
+#Writing to a file named "output.txt"
+print("Vulnerable IP's written to file 'output.txt'")
+
+with open("output.txt", "r") as f:
+    text = f.read()
+#Filter for only getting the IP's out of the JSON data
+ip_pattern = re.compile(r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b")
+ip_addresses = re.findall(ip_pattern, text)
+
+# Writing it again to output.txt
+with open("output.txt", "w") as f:
+    for ip in ip_addresses:
+        text = f.write(ip + "\n")
